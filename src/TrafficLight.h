0529a53e190e56eddf1e3dec4ed819fce9d82da9
Pusca Ghenadie <pusca.ghenadie@gmail.com>
2021-03-12 13:33:42 +0000

    Initial commit and FP.1

---
+++ src/TrafficLight.h
@@ -0,0 +1,60 @@
+#ifndef TRAFFICLIGHT_H
+#define TRAFFICLIGHT_H
+
+#include <mutex>
+#include <deque>
+#include <condition_variable>
+#include "TrafficObject.h"
+
+// forward declarations to avoid include cycle
+class Vehicle;
+
+
+// FP.3 Define a class „MessageQueue“ which has the public methods send and receive. 
+// Send should take an rvalue reference of type TrafficLightPhase whereas receive should return this type. 
+// Also, the class should define an std::dequeue called _queue, which stores objects of type TrafficLightPhase. 
+// Also, there should be an std::condition_variable as well as an std::mutex as private members. 
+
+template <class T>
+class MessageQueue
+{
+public:
+
+private:
+    
+};
+
+// Trafic light phase types
+enum TrafficLightPhase
+{
+    red,
+    green,
+};
+
+class TrafficLight
+{
+public:
+    // constructor / desctructor
+    TrafficLight();
+
+    // getters / setters
+    TrafficLightPhase getCurrentPhase();
+
+    // typical behaviour methods
+    void waitForGreen();
+    void simulate();
+    
+private:
+    TrafficLightPhase _currentPhase;
+    // typical behaviour methods
+    void cycleThroughPhases();
+    
+    // FP.4b : create a private member of type MessageQueue for messages of type TrafficLightPhase 
+    // and use it within the infinite loop to push each new TrafficLightPhase into it by calling 
+    // send in conjunction with move semantics.
+
+    std::condition_variable _condition;
+    std::mutex _mutex;
+};
+
+#endif
